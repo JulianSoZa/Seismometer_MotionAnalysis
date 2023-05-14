f9de1bcc86388d66d462142071ce7d255d147304
Julian Solano <juliansoza19@gmail.com>
2023-05-13 23:40:51 -0500

    Se actualizan las variables

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ from scipy.interpolate import make_interp_spline
 COM = 'COM7'
 arduinoSerial = serial.Serial(COM, 9600)
 
-n = 400  # Número de intervalos
+n = 400  # Número de muestras
 
 voltage = np.repeat(0.0,n)
 position = np.repeat(0.0,n)
@@ -19,29 +19,29 @@ tSpan = np.repeat(0.0,n)
 tFix = np.repeat(0.0,n)
 
 magneticField = 8E-3
-espiras = 1515
+spirals = 1515
 length = 2*3.14159*7E-3
 
 mass = 12E-3
 
 #Filtro del ruido ---------------------------------------------------
 values = 400
-rui = 0
+noise = 0
 
 for i in range(values):
     try:
         sensorValue = (float(arduinoSerial.readline().decode('utf-8')))
     except:
         sensorValue = 0
-    rui = rui + sensorValue
+    noise = noise + sensorValue
 
-meanRuido = rui/values
+noiseMean = noise/values
 
 #Lectura de los datos ------------------------------------------------
 print('Comienza')
 for i in range(len(voltage)):
     try:
-        voltage[i] = (float(arduinoSerial.readline().decode('utf-8'))) - meanRuido
+        voltage[i] = (float(arduinoSerial.readline().decode('utf-8'))) - noiseMean
         tFix[i] = time.time()
         tSpan[i] = time.time() - tFix[0]
     except:
@@ -50,23 +50,22 @@ for i in range(len(voltage)):
         tFix[i] = time.time()
         tSpan[i] = time.time() - tFix[0]
 print('Termina')
-tiempoT = tFix[n-1]-tFix[0]
-print('Tiempo: ', tiempoT)
+totalTime = tFix[n-1]-tFix[0]
+print('Tiempo: ', totalTime)
 
 #Cinematica del movimiento ------------------------------------------
-t = np.linspace(0.0, tiempoT, n)  # Intervalo de tiempo en segundos
-dt = tiempoT/n  # Espaciado, 16 puntos por período
-velocity = (voltage)/(magneticField*espiras*length*1000)
-constP = sum(velocity)/len(velocity)
-velocityN = velocity - constP
+timeValues = np.linspace(0.0, totalTime, n)  # Intervalo de tiempo en segundos
+dt = totalTime/n  # Espaciado, 16 puntos por período
+velocity = (voltage)/(magneticField*spirals*length*1000)
+offsetVelocity = velocity - (sum(velocity)/len(velocity))
 
-position[0] = velocityN[0]
-for i in range(len(velocityN)-1):
-    position[i+1] = position[i] + velocityN[i+1]
+position[0] = offsetVelocity[0]
+for i in range(len(offsetVelocity)-1):
+    position[i+1] = position[i] + offsetVelocity[i+1]
 
 acceleration[0] = 0
 for i in range(len(velocity)-1):
-     acceleration[i+1] = (velocity[i+1] - velocity[i]) / (t[i+1] - t[i])
+     acceleration[i+1] = (velocity[i+1] - velocity[i]) / (timeValues[i+1] - timeValues[i])
 
 ##Transformada de Fourier DFT --------------------------------------
 
@@ -87,14 +86,14 @@ print('La frecuencia de mayor amplitud es: ', fHz)
 
 fig,((ax,ax1)) = plt.subplots(2,1)
 
-ax.plot(t, velocity)
-ax.scatter(t, velocity, s = 12, c = 'black')
+ax.plot(timeValues, velocity)
+ax.scatter(timeValues, velocity, s = 12, c = 'black')
 ax.set_xlim(0, 1)
 ax.set_xlabel('Tiempo (s)')
 ax.set_ylabel('Velocidad (m/s)')
 ax.grid()
 
-ax1.plot(t, acceleration)
+ax1.plot(timeValues, acceleration)
 ax1.set_xlim(0, 1)
 ax1.set_xlabel('Tiempo (s)')
 ax1.set_ylabel('Aceleracion (m/s^2)')
@@ -108,7 +107,7 @@ ax2.set_xlabel('Frecuencia (Hz)')
 ax2.set_ylabel('F(w)')
 ax2.grid()
 
-ax3.plot(t, position)
+ax3.plot(timeValues, position)
 ax3.set_xlim(0, 1)
 ax3.set_xlabel('Tiempo (s)')
 ax3.set_ylabel('Posición (m)')
